6eca2cb98fbd9ab9319710b96d474454246636b5
seriouswill <73595384+seriouswill@users.noreply.github.com>
2021-05-27 08:24:20 +0100

    init

---
+++ views.py
@@ -0,0 +1,36 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d5ffa9d74f704f066279ca36b6a01fd6'
+    url2 = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d5ffa9d74f704f066279ca36b6a01fd6'
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+        r2 = requests.get(url2.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+            "temp_f": r2['main']['temp']
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/weather.html', context)
